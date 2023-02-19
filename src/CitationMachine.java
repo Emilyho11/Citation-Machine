ce6134e79a276ca400369440a2c5beac66032dbc
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2023-02-18 19:41:40 -0500

    Update CitationMachine.java

--- src/CitationMachine.java
+++ src/CitationMachine.java
@@ -1,31 +1,44 @@
+
 import java.util.*;
 
 public class CitationMachine {
 	static Scanner reader = new Scanner (System.in);
 	
+	public static String citationStyle() {
+		//Ask user what citation style they want
+		System.out.println("\nWhich citation style would you like to do (mla, apa, chicago)? Enter \"exit\" to quit the program: ");
+		String style = reader.nextLine();
+		return style;
+	}
+	
 	public static void main(String[]args) {
 		
 		//Ask user if they want to continue program
 		while(true) {
-			//Ask user what citation style they want
-			System.out.println("\nWhich citation style would you like to do (mla, apa, chicago)? Enter \"exit\" to quit the program: ");
-			String style = reader.nextLine();
+			String style = citationStyle();
 			
 			if (style.equalsIgnoreCase("exit")) {
 				System.out.println("Ok. Exiting the program.");
+				reader.close();
 				System.exit(0);
 			}
 			//if user types mla
 			else if(style.equalsIgnoreCase("mla")) {
-				mlaProgram.mla();
+				mlaProgram mlaCitation = new mlaProgram();
+				mlaCitation.instructions();
+				mlaCitation.mla();
 			}
 			//else if user types apa
 			else if(style.equalsIgnoreCase("apa")) {
-				apaProgram.apa();
+				apaProgram apaCitation = new apaProgram();
+				apaCitation.instructions();
+				apaCitation.apa();
 			}
 			//else if user types chicago
 			else if(style.equalsIgnoreCase("chicago")) {
-				chicagoProgram.chicago();
+				chicagoProgram chicagoCitation = new chicagoProgram();
+				chicagoCitation.instructions();
+				chicagoCitation.chicago();
 			}
 		}
 	}
